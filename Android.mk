73d3f981f7b7a3cddd9a0ea954e6fe7fa179c05f
Cao Jian <jian.cao@amlogic.com>
2021-03-05 20:32:26 +0800

    osd: add gdc_chip_check in libgdc [1/1]
    PD#SWPL-43941
    
    Problem:
    add gdc_chip_check in libgdc
    
    Solution:
    add code and test
    
    Verify:
    t7
    
    Signed-off-by: Cao Jian <jian.cao@amlogic.com>
    Change-Id: If1948d59f3862943c8c7fc42011df1e128e76f40

--- Android.mk
+++ Android.mk
@@ -78,3 +78,27 @@ ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
 LOCAL_PROPRIETARY_MODULE := true
 endif
 include $(BUILD_EXECUTABLE)
+
+# gdc_chip_check
+include $(CLEAR_VARS)
+LOCAL_PRELINK_MODULE:= false
+
+LOCAL_ARM_MODE := arm
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES:= gdc_chip_check.c
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include \
+	system/core/libion/include/ \
+	system/core/libion/kernel-headers \
+	system/core/liblog/include
+LOCAL_SHARED_LIBRARIES := liblog libion libgdc
+
+LOCAL_CFLAGS += -g
+LOCAL_CPPFLAGS := -g
+
+LOCAL_MODULE := gdc_chip_check
+
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+LOCAL_PROPRIETARY_MODULE := true
+endif
+include $(BUILD_EXECUTABLE)
